401737d263b326f5957bae0f3230f346afa094e2
Ivan Vigasin <ivigasin@gmail.com>
2018-01-22 19:41:22 -0800

    Fix ports

--- update_hack_rule.sh
+++ update_hack_rule.sh
@@ -3,11 +3,11 @@
 exec &>> /var/log/update_hack_rule.log
 echo Runnning at $(date)..
 
-NODEPORT_CHAIN=$(/sbin/iptables -t nat -vnL KUBE-NODEPORTS | awk '/tcp dpt:30365/ {print $3}')
+NODEPORT_CHAIN=$(/sbin/iptables -t nat -vnL KUBE-NODEPORTS | awk '/tcp dpt:3365/ {print $3}')
 
 [ -z "$NODEPORT_CHAIN" ] && exit 0
 
-read HACK_CHAIN HACK_RULE_NO <<< "$(/sbin/iptables -t nat -vnL PREROUTING | awk '/tcp dpt:30345/ {print $3, NR-2}')"
+read HACK_CHAIN HACK_RULE_NO <<< "$(/sbin/iptables -t nat -vnL PREROUTING | awk '/tcp dpt:3345/ {print $3, NR-2}')"
 
 [ "$NODEPORT_CHAIN" = "$HACK_CHAIN" ] && exit 0
 
@@ -18,4 +18,4 @@ then
     /sbin/iptables -t nat -D PREROUTING $HACK_RULE_NO
 fi
 
-/sbin/iptables -t nat -I PREROUTING 1 -p tcp --dport 30345 -j $NODEPORT_CHAIN
+/sbin/iptables -t nat -I PREROUTING 1 -p tcp --dport 3345 -j $NODEPORT_CHAIN
